7c36d7df5faf765477a1a687e2ba78817100e802
Pepper <imadori_wow@hotmail.com>
2016-09-28 18:10:11 -0500

    Added brand pages

--- js/main.js
+++ js/main.js
@@ -29,6 +29,39 @@
   		autoplaySpeed: 2000
 	  });
 
+	  $('.products--carrousel').slick({
+	  	nextArrow: '<button class="right"><svg><use xlink:href="#right" /></svg></button>',
+	  	prevArrow: '<button class="left"><svg><use xlink:href="#right" /></svg></button>',
+	  	slidesToShow: 6,
+	  	slidesToScroll: 1,
+	  	draggable: false,
+	  	responsive: [
+			{
+		      breakpoint: 480,
+		      draggable: true,
+		      settings: {
+		        slidesToShow: 1
+		    	}
+		    }
+		]
+	  });
+
+	  $('.vertical-slider').slick({
+	  	vertical: true,
+	  	slidesToShow: 4,
+	  	slidesToScroll: 1,
+	  	prevArrow: null,
+	  	infinite: true,
+	  	verticalSwiping: true,
+	  	nextArrow: '<button><svg><use xlink:href="#left" /></svg></button>',
+	  });
+
+		$('.vertical-slider').on('afterChange', function (event, slick, currentSlide) {
+			var attr = $(slick.$slides.get(currentSlide)).children('img').attr('src');
+			$('#change-on-slide').attr('src', attr);
+			console.log(attr);
+		});
+
 	  $('.spec__item').hover(
 	  	function() {
 	  		$(this).children().addClass('show animated rotateInUpLeft');
@@ -37,6 +70,10 @@
 	  		$(this).children().removeClass('show');
 	  	}
 	  )
+
+	  $('.single-product__subtitle').click(function() {
+	  	$(this).next().toggle('300');
+	  });
 
 	});
 })();
